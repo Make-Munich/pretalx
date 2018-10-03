73e8e87896bae08086c2d616ee255ac0c42a97ee
derchris <derchris@me.com>
2018-10-03 21:53:58 +0200

    modify clean

--- src/clean.py
+++ src/clean.py
@@ -1,12 +1,18 @@
-﻿from django.contrib.auth.models import User
-import os
-username = os.getenv('DJANGO_SU_NAME')
-password = os.getenv('DJANGO_SU_PASSWORD')
-email = os.getenv('DJANGO_SU_EMAIL')
+﻿from pretalx.event.utils import create_organiser_with_user
+from pretalx.person.models import User
+from os import getenv
 
-if User.objects.filter(username=username).count()==0:
-    User.objects.create_superuser(username, email, password)
+password = getenv('DJANGO_SU_PASSWORD')
+email = getenv('DJANGO_SU_EMAIL')
+
+if User.objects.filter(email=email).count()==0:
+    User.objects.create_superuser(email, password)
     print('Superuser created.')
 else:
     print('Superuser creation skipped.')
 
+user = User.objects.order_by('-id').filter(is_administrator=True).first()
+organiser_name = 'Make Munich Organiser'
+organiser_slug = 'mmorg'
+
+create_organiser_with_user(name=organiser_name, slug=organiser_slug, user=user)
