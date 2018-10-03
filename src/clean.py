56081d6eca3f34c961156eb80dd3763ffa6e2493
derchris <derchris@me.com>
2018-10-03 21:25:05 +0200

    add clean script for su

---
+++ src/clean.py
@@ -0,0 +1,12 @@
+﻿from django.contrib.auth.models import User
+import os
+username = os.getenv('DJANGO_SU_NAME')
+password = os.getenv('DJANGO_SU_PASSWORD')
+email = os.getenv('DJANGO_SU_EMAIL')
+
+if User.objects.filter(username=username).count()==0:
+    User.objects.create_superuser(username, email, password)
+    print('Superuser created.')
+else:
+    print('Superuser creation skipped.')
+
